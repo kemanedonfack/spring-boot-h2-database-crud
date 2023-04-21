4c743e8c1e2fc021896cc837ba26818ae860dcd1
Kemane AMH <kdonfack@amhconsulting-group.com>
2023-04-21 15:32:23 +0100

    update

--- src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
+++ src/main/java/com/bezkoder/spring/jpa/h2/SpringBootJpaH2Application.java
@@ -1,13 +1,31 @@
 package com.bezkoder.spring.jpa.h2;
 
+import com.bezkoder.spring.jpa.h2.model.Tutorial;
+import com.bezkoder.spring.jpa.h2.repository.TutorialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootJpaH2Application {
+public class SpringBootJpaH2Application implements CommandLineRunner {
 
+	@Autowired
+	public TutorialRepository tutorialRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJpaH2Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Tutorial tutorial = new Tutorial("kind ", "kind description", false);
+		Tutorial tutorial1 = new Tutorial("kind 1", "kind description 2", false);
+		Tutorial tutorial2 = new Tutorial("kind 2", "kind description 3", false);
+
+		tutorialRepository.save(tutorial);
+		tutorialRepository.save(tutorial1);
+		tutorialRepository.save(tutorial2);
+
+	}
 }
